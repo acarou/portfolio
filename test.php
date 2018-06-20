80e05ea9e38ae92a257a48382ca8bbe6c2ea702d
quentin <quentin1.sommer@epitech.eu>
2018-06-20 23:09:13 +0200

    mail and fseuihfsiodfhiosefniozfg

--- test.php
+++ test.php
@@ -5,15 +5,17 @@
 </head>
 <body>
 	<?php 
+	error_reporting(E_ALL|E_STRICT);
 	$to = "quentin.sommer1@gmail.com";
 	$subject = "ton sujet";
 
-	$message="<html><head></head><body>ton contenu avec style à l'intérieur des balises</body></html>";
+	$message="Ceci est un message";
 
-	$headers  = 'MIME-Version: 1.0' . "\r\n";
-	$headers .= 'Content-type: text/html; charset=UTF-8' . "\r\n";
-
-	mail($to, $subject, $message, $headers);
+	if (mail($to, $subject, $message)) {
+		echo 'mail envoyer';
+	}
+	else
+		echo 'mail pas envoyer';
 	?>
 </body>
 </html>
